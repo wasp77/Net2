9657e4d1d95f1030cd593597411b6a8212c447fb
john dupuis <johndupuis@cs-wifi-129.cs.st-andrews.ac.uk>
2016-11-23 11:53:19 -0500

    working representation next adding post

--- src/ConnectionManager.java
+++ src/ConnectionManager.java
@@ -2,10 +2,7 @@ import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
-import java.net.MalformedURLException;
-import java.net.Socket;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
 import java.util.ArrayList;
 
 public class ConnectionManager extends Thread {
@@ -18,10 +15,9 @@ public class ConnectionManager extends Thread {
 
     public void run() {
         String reader;
-        String[] parser = null;
+        String[] parser;
         String url = null;
-        StringBuilder sb = new StringBuilder();
-        ArrayList<String> request = new ArrayList<String>();
+        String requestType = null;
 
         try {
             clientInput = new BufferedReader(new InputStreamReader(clientConnection.getInputStream()));
@@ -33,20 +29,26 @@ public class ConnectionManager extends Thread {
                         url = parser[i].replace("http", "https");
                     }
                 }
+                requestType = parser[0];
             }
-
-
         } catch (IOException e) {
             System.out.println("Problem reading from client: " + e);
         }
 
         URL requestUrl = null;
+
         try {
             if (url != null) {
                 requestUrl = new URL(url);
             }
         } catch (MalformedURLException e) {
             System.out.println("Incorrect URL: " + e);
+        }
+
+        if (requestType.equals("GET")) {
+            executeGet(requestUrl);
+        } else if (requestType.equals("POST")) {
+            executeGet(requestUrl);
         }
 
 
@@ -59,17 +61,19 @@ public class ConnectionManager extends Thread {
                 serverConnection = (HttpsURLConnection) con;
                 InputStream serverToClient = null;
                 int response = serverConnection.getResponseCode();
-                boolean redirect = false;
+
 
                 if (response == serverConnection.HTTP_OK) {
+                    System.out.println(response);
                     serverToClient = serverConnection.getInputStream();
                 } else if (response == serverConnection.HTTP_MOVED_TEMP || response == serverConnection.HTTP_MOVED_PERM ||
                         response == serverConnection.HTTP_SEE_OTHER){
                     System.out.println(response);
                     String redirectUrl = serverConnection.getHeaderField("Location");
-                    redirectUrl = redirectUrl.replace("http", "https");
-                    serverConnection = (HttpsURLConnection) new URL(redirectUrl).openConnection();
-                    serverToClient = serverConnection.getInputStream();
+                    HttpURLConnection redirectConnection = (HttpURLConnection) new URL(redirectUrl).openConnection();
+                    redirectConnection.setDoInput(true);
+                    serverToClient = redirectConnection.getInputStream();
+
                 }
 
 
@@ -85,20 +89,28 @@ public class ConnectionManager extends Thread {
                         writeToClient.write(buffer, 0, bytesRead);
                         writeToClient.flush();
                     }
+
                     serverToClient.close();
                 }
+
                 writeToClient.close();
                 serverConnection.disconnect();
+                clientConnection.close();
             }
 
             clientInput.close();
-            clientConnection.close();
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Problem reading from server to client: " + e);
         }
+    }
 
-
+    public void executeGet(URL requestUrl) {
 
     }
+
+    public void executePost(URL requestUrl) {
+
+    }
+
 }
